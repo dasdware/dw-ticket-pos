9b4d248af971e0fabcc4d0cd226c34d7e45db647
dasdware <info@dasdware.de>
2019-03-16 16:50:29 +0100

    moved EventJournalEntriesList to own file

--- lib/views/event_journal.dart
+++ lib/views/event_journal.dart
@@ -1,4 +1,5 @@
 import 'package:dw_ticket_pos/model/event_journal_entry.dart';
+import 'package:dw_ticket_pos/widgets/event_journal_entries_list.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -50,28 +51,5 @@ class EventJournalView extends StatelessWidget {
             child: Icon(Icons.add),
           ),
         ));
-  }
-}
-
-class EventJournalEntriesList extends StatelessWidget {
-  const EventJournalEntriesList({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ScopedModelDescendant<EventJournal>(
-      builder: (context, _, model) => ListView(
-            children: model.entries.reversed
-                .map((entry) => ListTile(
-                      title: Text(formatDateTime(entry.timestamp)),
-                      subtitle: Text(formatTicketEntries(entry)),
-                      trailing: Text(formatPrice(entry.price),
-                          style: TextStyle(
-                              fontSize: 22.0, fontWeight: FontWeight.bold)),
-                    ))
-                .toList(),
-          ),
-    );
   }
 }
---
+++ lib/widgets/event_journal_entries_list.dart
@@ -0,0 +1,28 @@
+import 'package:dw_ticket_pos/model/event_journal.dart';
+import 'package:dw_ticket_pos/utils/format.dart';
+
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class EventJournalEntriesList extends StatelessWidget {
+  const EventJournalEntriesList({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModelDescendant<EventJournal>(
+      builder: (context, _, model) => ListView(
+            children: model.entries.reversed
+                .map((entry) => ListTile(
+                      title: Text(formatDateTime(entry.timestamp)),
+                      subtitle: Text(formatTicketEntries(entry)),
+                      trailing: Text(formatPrice(entry.price),
+                          style: TextStyle(
+                              fontSize: 22.0, fontWeight: FontWeight.bold)),
+                    ))
+                .toList(),
+          ),
+    );
+  }
+}
