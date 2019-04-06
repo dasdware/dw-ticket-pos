ce5a426f1d180a423eccbbacaba867e783eca155
dasdware <info@dasdware.de>
2019-04-06 14:51:48 +0200

    restructured main app so that it can keep the storage state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:dw_ticket_pos/model/storage.dart';
 import 'package:dw_ticket_pos/views/storage_home.dart';
 import 'package:intl/date_symbol_data_local.dart' as intl_local_date_data;
@@ -9,51 +8,44 @@ import 'package:scoped_model/scoped_model.dart';
 import 'package:dw_ticket_pos/model/mock_storage.dart';
 import 'package:dw_ticket_pos/utils/format.dart';
 
+Future<Storage> loadStorage() {
+  return Future<Storage>.delayed(
+    Duration(seconds: 2),
+    () => MockStorage(),
+  );
+}
+
 Future<Null> main() async {
   await intl_local_date_data.initializeDateFormatting();
   initFormats();
-  // debugPaintSizeEnabled = true;
-  runApp(DWTicketPosApp());
+  runApp(DWTicketPosApp(loadStorage()));
 }
 
 class DWTicketPosApp extends StatelessWidget {
-  Future<Storage> loadEvent(BuildContext context) {
-    return Future<Storage>.delayed(
-      Duration(seconds: 2),
-      () => MockStorage(),
-    );
-  }
+  final Future<Storage> storageLoader;
+
+  DWTicketPosApp(this.storageLoader);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Storage>(
-        future: loadEvent(context),
+    return MaterialApp(
+      title: 'DW Ticket Verkaufspunkt',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder<Storage>(
+        future: this.storageLoader,
         builder: (context, storage) {
           if (!storage.hasData) {
-            print("No data! " + new DateTime.now().toString());
             return Center(child: CircularProgressIndicator());
           } else {
-            print("Data! " + new DateTime.now().toString());
             return ScopedModel<Storage>(
               model: storage.data,
-              child: MaterialApp(
-                title: 'Flutter Demo',
-                theme: ThemeData(
-                  // This is the theme of your application.
-                  //
-                  // Try running your application with "flutter run". You'll see the
-                  // application has a blue toolbar. Then, without quitting the app, try
-                  // changing the primarySwatch below to Colors.green and then invoke
-                  // "hot reload" (press "r" in the console where you ran "flutter run",
-                  // or simply save your changes to "hot reload" in a Flutter IDE).
-                  // Notice that the counter didn't reset back to zero; the application
-                  // is not restarted.
-                  primarySwatch: Colors.blue,
-                ),
-                home: StorageHomeView(),
-              ),
+              child: StorageHomeView(),
             );
           }
-        });
+        },
+      ),
+    );
   }
 }
